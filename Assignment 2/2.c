5f58153199945ed5548aa2677a2a0798d2af13c1
Ekta Chaudhari <90906114+ekta028@users.noreply.github.com>
2021-10-06 22:56:17 +0530

    Write a program which display all palindrome elements of singly linked list in C

---
+++ Assignment 2/2.c
@@ -0,0 +1,85 @@
+//Write a program which display all palindrome elements from singly linear linked list.
+
+//Output : 464 101
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int Data;
+	struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE Head,int no)
+{
+	PNODE newn = NULL;
+	
+	newn = (PNODE)malloc(sizeof(NODE));
+	newn->Data = no;
+	newn->Next = NULL;
+	
+	if(*Head == NULL)
+	{
+		*Head = newn;
+	}
+	else
+	{
+		newn->Next = *Head;
+		*Head = newn;
+	}
+}
+	
+void Display(PNODE Head)
+{
+	while(Head != NULL)
+	{
+		printf("|%d| -> ",Head->Data);
+		Head = Head->Next;
+	}
+	printf("NULL\n");
+}
+
+int DisplayPalindrome(PNODE Head)
+{
+	PNODE temp = Head;
+	int iSum = 0 , iRem = 0 , iRev = 0 , iValue = 0;
+	
+	while(temp != NULL)
+	{
+		iRev = 0;
+		iValue = temp->Data;
+		while(iValue != 0)
+		{
+			iRem = iValue % 10;
+			iRev = (iRev * 10)+iRem;
+			iValue = iValue / 10;
+		}
+		if(iRev == temp->Data)
+		{
+			printf("%d\t",temp->Data);
+		}
+		temp = temp->Next;
+	}
+}
+	
+int main()
+{
+	PNODE First = NULL;
+	int iRet = 0;
+	
+	InsertFirst(&First,101);
+	InsertFirst(&First,289);
+	InsertFirst(&First,464);
+	InsertFirst(&First,547);
+	InsertFirst(&First,192);
+	
+	DisplayPalindrome(First);
+	
+	return 0;
+}
